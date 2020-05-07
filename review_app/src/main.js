d16c8814d463454b256565ce751697cc27c111bb
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-07 16:22:13 +0900

    [復習]vuetifyのインストール+コンポーネント作成

--- review_app/src/main.js
+++ review_app/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+
+  // htmlファイルのid appではなく、vueの方
+  render: h => h(App)
 }).$mount('#app')
