7b14ef8384ae08b254952649f29f67aff929753c
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-08 15:48:15 +0900

    [復習]slotの理解

--- review_app/src/App.vue
+++ review_app/src/App.vue
@@ -5,16 +5,25 @@
       <h2>{{ greet }}</h2>
       <h2>{{ message }}</h2>
     </v-content>
+    <v-content>
+      <TestComponent>
+        <li slot="a">
+          通常はここのテキストは消えます。嵌め込んで利用するにはslotを使います。
+        </li>
+      </TestComponent>
+    </v-content>
   </v-app>
 </template>
 
 <script>
   import HelloWorld from './components/HelloWorld';
+  import TestComponent from './components/TestComponent'
 
   export default {
     name: 'App',
     components: {
       HelloWorld,
+      TestComponent
     },
     data: function () {
       return {
---
+++ review_app/src/components/TestComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        {{ test }}
+        <ul>
+          <slot name="a" />
+        </ul>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  // name: 'TestComponent',
+  data: function() {
+    return {
+      test: "Hello!"
+    }
+  }
+}
+</script>
